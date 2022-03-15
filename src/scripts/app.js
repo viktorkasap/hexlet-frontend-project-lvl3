8dd58daf20a06aa53ba2e284a2fbe0b332cb7d9b
Victor Kasap <kasap.victor@gmail.com>
2022-03-15 22:05:54 +0300

    feat: added clear errors

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,70 +5,103 @@ import isEmpty from 'lodash/isEmpty';
 import api from './api';
 import render from './render';
 
-const schema = yup.object().shape({
-  url: yup.string()
-    .url('Некорректный адрес URL')
-    // .notOneOf(urls)
-    .required('Не должно быть пустым'),
+const errorMessages = {
+	network: {
+		error: {
+			request:  'Ресурс не доступен',
+			default: 'Неизвестная ошибка. Что-то пошло не так.',
+		},
+	},
+};
+
+const schema = (urls) => yup.object().shape({
+	url: yup.string()
+		.url('Некорректный адрес URL')
+		.notOneOf(urls, 'URL уже существует')
+		.required('Поле не должно быть пустым'),
 });
 
-const validate = (fields) => schema // const validate = (fields, urls) => schema
-  .validate(fields, { abortEarly: false })
-  .then(() => ({}))
-  .catch((err) => keyBy(err.inner, 'path'));
+const validate = (fields, urls) => schema(urls) // const validate = (fields, urls) => schema
+	.validate(fields, {abortEarly: false})
+	.then(() => ({}))
+	.catch((err) => keyBy(err.inner, 'path'));
 
 export default () => {
-  const elements = {
-    form: document['form-search'],
-    fields: {
-      url: () => elements.form.elements.url,
-    },
-    submit: () => elements.form.elements.submit,
-    errors: {
-      url: document.querySelector('.text-danger__url'),
-    },
-  };
+	const elements = {
+		form: document['form-search'],
+		fields: {
+			url: () => elements.form.elements.url,
+		},
+		submit: () => elements.form.elements.submit,
+		errors: {
+			url: document.querySelector('.text-danger__url'),
+		},
+	};
+	
+	const state = {
+		form: {
+			valid: null,
+			process: {
+				state: 'filling',
+				error: null,
+			},
+			fields: {
+				url: null,
+			},
+			errors: {},
+		},
+		urls: [],
+		content: [],
+	};
+	
+	const watchedState = onChange(state, render(elements));
+	
+	elements.form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const formData = new FormData(elements.form);
+		
+		// FILL FIELDS VALUES OF STATE
+		Object.entries(elements.fields).forEach(([name, element]) => {
+			watchedState.form.fields[name] = formData.get(name).trim();
+		});
+		
+		// ERRORS
+		const errors = validate(state.form.fields, watchedState.urls);
+		errors.then((data) => {
+			watchedState.form.errors = data;
+			watchedState.form.valid = isEmpty(data);
+			
+			// AXIOS GET DATA OF LINK
+			if (watchedState.form.valid) {
+				// + TODO #1 вытащить из апи изменения стейтов
+				// + TODO #2 сделать список урлов
+				// + TODO #3 валидаця на наличие ура в списке урлов
+				
+				const url = watchedState.form.fields.url;
+				api(url)
+					.then((response) => {
+						watchedState.form.process.state = 'sending';
+						watchedState.form.process.error = null;
+						return response.data;
+					})
+					.then((data) => {
+						watchedState.content = [...state.content, data];
+						watchedState.urls = [...watchedState.urls, url];
+						watchedState.form.process.state = 'sent';
+						console.log(state);
+					})
+					.catch((err) => {
+						watchedState.form.process.state = 'error';
 
-  const state = {
-    form: {
-      valid: null,
-      process: {
-        state: 'filling',
-        error: null,
-      },
-      fields: {
-        url: null,
-      },
-      errors: {},
-    },
-    content: [],
-  };
-
-  const watchedState = onChange(state, render(elements));
-
-  elements.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // FILL FIELDS VALUES OF STATE
-    Object.entries(elements.fields).forEach(([name, element]) => {
-      const value = element().value.trim(); // TODO заменить на FORMDATA
-      watchedState.form.fields[name] = value;
-    });
-
-    // ERRORS
-    const errors = validate(state.form.fields);
-    errors.then((data) => {
-      watchedState.form.errors = data;
-      watchedState.form.valid = isEmpty(data);
-
-      // AXIOS GET DATA OF LINK
-      if (watchedState.form.valid) {
-        // TODO #1 вытащить из апи изменения стейтов
-        // TODO #2 сделать список урлов
-        // TODO #3 валидаця на наличие ура в списке урлов
-        
-        api(watchedState, watchedState.form.fields.url);
-      }
-    });
-  });
+						if (err.request) {
+							console.log('REQUEST\n', errorMessages.network.error.request);
+							watchedState.form.process.error = errorMessages.network.error.request;
+						} else {
+							console.log('MESSAGE\n', err.message);
+							watchedState.form.process.error = errorMessages.network.error.default;
+						}
+					});
+			}
+		});
+	});
 };
--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,9 +1,22 @@
 import isEqual from 'lodash/isEqual';
 
+const clearErrors = (elements) => {
+  const {form} = elements;
+  const {fields} = elements;
+
+  Object.entries(fields).forEach(([name, el]) => {
+    // REMOVE CLASS ERROR
+    el().classList.remove('is-invalid');
+
+    // HIDE ERRORS
+    const errorEl = elements.errors[name];
+    errorEl.innerHTML = '';
+  });
+}
+
 const renderErrors = (elements, value, prevValue) => {
   const { form } = elements;
-  const input = elements.fields.url();
-
+  
   Object.entries(value).forEach(([name, error]) => {
     // ADD CLASS ERROR
     elements.fields[name]().classList.add('is-invalid');
@@ -17,18 +30,27 @@ const renderErrors = (elements, value, prevValue) => {
 
 export default (elements) => (path, value, prevValue) => {
   // path form.fields.url
-  console.log('PATH --->', path);
-  console.log('VALUE --->', value);
-  console.log('PREV VALUE --->', prevValue);
-  console.log('\n---\n');
-
+  // console.log('PATH --->', path);
+  // console.log('VALUE --->', value);
+  // console.log('PREV VALUE --->', prevValue);
+  // console.log('\n---\n');
+  
   // path form.errors
   switch (true) {
     case path === 'form.errors' && !isEqual(value, prevValue):
+      console.log('RENDER ERRORS')
       renderErrors(elements, value, prevValue);
+      break;
+      
+    case value === 'sent':
+      clearErrors(elements);
       break;
 
     default:
       break;
   }
+  
+
+  
+  
 };

c03752017f7de9111f47eb086ba9f9a8ae239d4d
Victor Kasap <kasap.victor@gmail.com>
2022-03-23 06:18:09 +0300

    feat step8: refactor code

---
+++ src/scripts/app.old.js
@@ -0,0 +1,69 @@
+import onChange from 'on-change';
+import i18n from 'i18next';
+import render from './view/render.js';
+import formHandler from './controllers/form.js';
+import postsHandler from './controllers/posts.js';
+import resources from './locales/index.js';
+import updateRss from './api/updateRss.js';
+
+export default () => {
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    fields: {
+      url: document.getElementById('url-input'),
+    },
+    submit: document.getElementById('submit'),
+    message: document.querySelector('.text-danger'),
+    posts: document.querySelector('.posts'),
+    feeds: document.querySelector('.feeds'),
+    modal: {
+      title: document.querySelector('.modal-title'),
+      body: document.querySelector('.modal-body'),
+      link: document.querySelector('.full-article'),
+    },
+  };
+
+  const state = {
+    lng: 'ru',
+    form: {
+      valid: null,
+      fields: {
+        url: null,
+      },
+      process: {
+        info: null,
+        status: null,
+      },
+    },
+    urls: [],
+    feeds: [],
+    ui: {
+      viewedPostsIds: [],
+      modal: {
+        renderId: null,
+      },
+    },
+    update: {
+      interval: 5000,
+      isUpdate: null,
+    },
+  };
+
+  const i18nInstance = i18n.createInstance();
+  i18nInstance.init({
+    lng: state.lng,
+    debug: true,
+    resources,
+  });
+
+  const { form: formEl, posts: postsEl } = elements;
+  const watchedState = onChange(state, render(state, elements, i18nInstance));
+
+  formEl.addEventListener('submit', (e) => {
+    formHandler(e, formEl, elements, watchedState, i18nInstance);
+  });
+  postsEl.addEventListener('click', (e) => {
+    postsHandler(e, elements, watchedState);
+  });
+  setTimeout(() => updateRss(watchedState, i18nInstance), state.update.interval);
+};
